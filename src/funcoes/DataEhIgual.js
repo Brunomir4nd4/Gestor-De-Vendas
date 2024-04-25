0051140a286a297577013afdeb05cfb19cc872de
bmb40 <bmb400289@gmail.com>
2024-04-25 13:59:02 -0300

    Alteração tabela cliente

--- src/funcoes/DataEhIgual.js
+++ src/funcoes/DataEhIgual.js
@@ -1,21 +1,26 @@
 export function DataEhIgual(clientes, index) {
     let size = clientes.length;
-    let proximoIndex = index + 1;
-    if (proximoIndex < size -1) {
-        if (clientes[index].data === clientes[proximoIndex].data) {
+    let indexAnterior = index - 1;
+    if (index == 0) {
+        return true;
+    }
+    else if (index < size -1) {
+        if (clientes[indexAnterior].data === clientes[index].data) {
             return true;
         } else {
             return false;
         }
     }
-    else if (size > 1){
-        if (clientes[index].data === clientes[index-1].data) {
-            return true;
+    else if (index == size -1){
+        if (index > 0) {
+            if (clientes[index].data === clientes[indexAnterior].data) {
+                return true;
+            } else {
+                return false;
+            }
+
         } else {
-            return false;
+            return true;
         }
-    }
-    else {
-    return false;
     }
 }
